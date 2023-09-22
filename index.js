44edc230f1a7122f27c4574ecee6a5b377553061
Pedro-PAM <pedropamrj@gmail.com>
2023-09-22 19:24:55 -0300

    Changes on all code files

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const texto = ["evolução","desenvolvimento humano","plenitude pelo esporte"]
+document.addEventListener('DOMContentLoaded', function ()
+    {const texto = ["evolução","desenvolvimento humano","plenitude pelo esporte"]
 const text = document.querySelector(".textos");
 function exibirTexto(){    
     setTimeout(() =>{
@@ -17,3 +18,23 @@ exibirTexto();
 
 
 setInterval(exibirTexto,6000);
+
+const ul = document.querySelector(".sobre") //pega todos os elementos com a classe sobre
+
+//Observador que adiciona a propriedade "mostrar" ao elemento, fazendo uma animação dele.
+
+const observer = new IntersectionObserver(entries=>{
+    entries.forEach(entry =>{
+        const intersecting = entry.isIntersecting;        
+        if (intersecting){
+
+            setTimeout(() =>{
+                entry.target.classList.add("show");
+                observer.unobserve(entry.target);},
+                800);
+        }
+    }),
+    { threshold: [0, 0.25, 0.5, 0.75, 1] }
+})
+
+observer.observe(ul);})
